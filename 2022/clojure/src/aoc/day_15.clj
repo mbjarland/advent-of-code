185ef8680781eec6f1767ac248533254e538a2e0
Matias Bjarland <matias@iteego.com>
2023-01-05 14:53:50 +0100

    adding working day 15...still takes 17 seconds

--- 2022/clojure/src/aoc/day_15.clj
+++ 2022/clojure/src/aoc/day_15.clj
@@ -1,55 +1,45 @@
-(ns aoc.day-15
-  (:require [clojure.string :refer [join]]))
-
-(def sample (join \newline ["Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
-
-                            "Sensor at x=9, y=16: closest beacon is at x=10, y=16"
-                            "Sensor at x=13, y=2: closest beacon is at x=15, y=3"
-                            "Sensor at x=12, y=14: closest beacon is at x=10, y=16"
-                            "Sensor at x=10, y=20: closest beacon is at x=10, y=16"
-                            "Sensor at x=14, y=17: closest beacon is at x=10, y=16"
-                            "Sensor at x=8, y=7: closest beacon is at x=2, y=10"
-                            "Sensor at x=2, y=0: closest beacon is at x=2, y=10"
-                            "Sensor at x=0, y=11: closest beacon is at x=2, y=10"
-                            "Sensor at x=20, y=14: closest beacon is at x=25, y=17"
-                            "Sensor at x=17, y=20: closest beacon is at x=21, y=22"
-                            "Sensor at x=16, y=7: closest beacon is at x=15, y=3"
-                            "Sensor at x=14, y=3: closest beacon is at x=15, y=3"
-                            "Sensor at x=20, y=1: closest beacon is at x=15, y=3"
-
-                            ]))
-;                   sf (fn [[n m]] (or (<= n g m) (<= n h m) (<= g n h) (<= g m h)))
-;                   [c d] (first (filter sf r))
-; pf - parse function, ls - lines, rf - range function, ws - walls
-(defn doit []
-  (let [d  (partition 2 (partition 2 (map parse-long (re-seq #"(?s)-?\d+" (slurp "data/2022_15")))))
-        df (fn [[a b] [c d]] (+ (abs (- a c)) (abs (- b d))))
-        rf (fn [y]
-             (let [if (fn [r [[sx sy :as s] b]]                  ; s - sensor, b - beacon, if - interval function
-                        (let [dx (- (df s b) (abs (- sy y)))]
-                          (if (<= 0 dx) (conj r [(- sx dx) (+ sx dx)]) r)))
-                   mf (fn [r [c d]]                              ; mf - merge function
+(ns aoc.day-15)
+; rf returns a collection of non-allowed intervals for beacons
+(let [d   (partition 2 (partition 2 (map parse-long (re-seq #"(?s)-?\d+" (slurp "data/2022_15")))))
+      df  (fn [a b c d] (+ (abs (- a c)) (abs (- b d))))         ; distance function
+      bf  #(set (keep (fn [[_ [bx by]]] ({% bx} by)) d))         ; x coords for beacons on row %
+      rf  (fn [eb? y]                                            ; row function - eb? - exclude beacons
+            (let [zf  (fn [[[sx sy] [bx by]]]                    ; zone function, s - sensor, b - beacon
+                        (let [dx (- (df sx sy bx by) (abs (- sy y)))]
+                          (when (<= 0 dx) [(- sx dx) (+ sx dx)])))
+                  mf  (fn [r [c d]]                              ; merge function
                         (let [[a b] (last r)]
-                          (if (and a (<= a c b))
-                            (conj (butlast r) [(min a c) (max b d)]) (conj r [c d]))))
-                   bx (set (keep (fn [[[_ _] [bx by]]] (when (= by y) bx)) d))
-                   ;bx (set (keep #(when (= y (nth (nth % 1) 1)) (nth (nth % 1) 0)) d))
-
-                   bf (fn [[a b]]
-                        (let [bi (sort (filter #(<= a % b) bx))]
+                          (if (and a (<= a c b)) (conj (butlast r) [(min a c) (max b d)])
+                            (conj r [c d]))))
+                  ef  (fn [[a b]]                                ; exclude beacons function
+                        (let [bi (sort (filter #(<= a % b) (bf y)))]
                           (map (fn [[c d]] [(inc c) (dec d)])
-                               (partition 2 1 (flatten [(dec a) bi (inc b)])))))]
-               (->> (sort (reduce if #{} d))
-                    (reduce mf [])
-                    (mapcat bf))))]
-    ;(prn :d d)
-    (prn {:one (reduce (fn [r [a b]] (+ r (- b a))) 0 (rf 2000000))
-          :two nil})
-    ;(prn {:one (count (rf 10)) :two nil})
+                               (partition 2 1 (flatten [(dec a) bi (inc b)])))))
+                  res (sort (reduce mf [] (sort (keep zf d))))]
+              (if eb? (mapcat ef res) res)))
+      xyf (fn [y] (let [f (fn [[[_ a] [b _]]] (map #(vector % y) (range (inc a) b)))]
+                    (some identity (mapcat f (partition 2 1 (rf false y))))))]
+  (prn {:one (reduce (fn [r [a b]] (+ r (- b a) 1)) 0 (rf true 2000000)) ;(rf 2000000))
+        :two (let [[x y] (some xyf (range 0 4000001))] (+ (* x 4000000) y))}))
 
-    ))
+; {:one 7730280, :two 10382630753392}
+; takes about 20s to complete
 
 (comment
-  (let [xr #(map (fn [[[sx _] [bx _]]] (%1 sx (abs (- sx bx)))) d)
-        [lx hx] [(apply min (xr -)) (apply max (xr +))]
-        ]))
+  (def sample (join \newline ["Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
+                              "Sensor at x=9, y=16: closest beacon is at x=10, y=16"
+                              "Sensor at x=13, y=2: closest beacon is at x=15, y=3"
+                              "Sensor at x=12, y=14: closest beacon is at x=10, y=16"
+                              "Sensor at x=10, y=20: closest beacon is at x=10, y=16"
+                              "Sensor at x=14, y=17: closest beacon is at x=10, y=16"
+                              "Sensor at x=8, y=7: closest beacon is at x=2, y=10"
+                              "Sensor at x=2, y=0: closest beacon is at x=2, y=10"
+                              "Sensor at x=0, y=11: closest beacon is at x=2, y=10"
+                              "Sensor at x=20, y=14: closest beacon is at x=25, y=17"
+                              "Sensor at x=17, y=20: closest beacon is at x=21, y=22"
+                              "Sensor at x=16, y=7: closest beacon is at x=15, y=3"
+                              "Sensor at x=14, y=3: closest beacon is at x=15, y=3"
+                              "Sensor at x=20, y=1: closest beacon is at x=15, y=3"
+
+                              ]))
+  )
